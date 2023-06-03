99131a9e3fb578fe46aba7e10b2e0b98dad3359a
harris-chris <chrisharriscjh@gmail.com>
2023-06-03 11:18:17 +0900

    Add another package

--- cpp-specific-toolchain/flake.nix
+++ cpp-specific-toolchain/flake.nix
@@ -12,10 +12,16 @@
 
     oldTools = {
       gcc830 = (import (builtins.fetchGit {
-         name = "gcc_8_3_0";
+         name = "gcc830";
          url = "https://github.com/NixOS/nixpkgs/";
          ref = "refs/heads/nixpkgs-unstable";
          rev = "a9eb3eed170fa916e0a8364e5227ee661af76fde";
+      }) { inherit system; }).gcc-unwrapped;
+      gcc920 = (import (builtins.fetchGit {
+         name = "gcc920";
+         url = "https://github.com/NixOS/nixpkgs/";
+         ref = "refs/heads/nixpkgs-unstable";
+         rev = "9d21fe813fd96afd4a08d5437186ebe438546693";
       }) { inherit system; }).gcc-unwrapped;
       glibc218 = (import (builtins.fetchGit {
          name = "glibc_2_18";
@@ -44,6 +50,8 @@
           gcc830 glibc218 origPkgs.stdenv origPkgs;
         gcc830Glibc237 = getCustomGccStdenv
           gcc830 origPkgs.glibc origPkgs.stdenv origPkgs;
+        gcc920Glibc237 = getCustomGccStdenv
+          gcc920 origPkgs.glibc origPkgs.stdenv origPkgs;
       };
 
     getOverlays = origPkgs:
